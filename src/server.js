2275edb63b24e7f0bd98908c00c4dbe932cadc1f
joaocastro <joaocastro0429@gmail.com>
2024-03-15 23:35:03 -0300

    create withdraw

--- src/server.js
+++ src/server.js
@@ -22,6 +22,18 @@ function verrifyAlreadyExists(request,response,next){
 
 }
 
+function balance(statement){
+   const balance= statement.reduce((acc,operation)=>{
+        if(operation.type=="credit"){
+            return acc+ operation.amount
+        }else{
+            acc- operation.amount
+        }
+
+    },0)
+    return balance
+}
+
 app.post("/account",(request,response)=>{
     const {cpf,name}=request.body
     const customerAlreadyExists=customers.some(customer=>customer.cpf===cpf)
@@ -51,15 +63,13 @@ app.get("/account",verrifyAlreadyExists,(request,response)=>{
 app.post("/deposit",verrifyAlreadyExists,(request,response)=>{
 
 
-    const {description,ammount}=request.body
+    const {description,amount}=request.body
     
     const {customer}=request
 
-
-
     const statementOperation={
         description,
-        ammount,
+        amount,
         create_at:new Date(),
         type:'credit'
     }
@@ -69,7 +79,28 @@ app.post("/deposit",verrifyAlreadyExists,(request,response)=>{
     return response.status(201).send()
 
 
-
 }) 
 
-app.listen(3333)
+app.post("/withdraw",verrifyAlreadyExists,(request,response)=>{
+    const {amount}=request.body
+
+    const {customer}=request
+
+    const getbalnce=balance(customer.statement)
+    if(getbalnce<amount){
+      return response.status(400).json({message:"insufficient funds !"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+      };
+
+
+      customer.statement.push(statementOperation)
+
+      return response.status(201).send()
+})
+
+app.listen(3333) 
